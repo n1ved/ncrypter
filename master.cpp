c673f57cbc8d62424a9b1248527887e7c82080ec
Nived.R.S <nivedrsalini@gmail.com>
2020-03-24 20:51:12 +0530

    v1.0

--- master.cpp
+++ master.cpp
@@ -1,3 +1,9 @@
+/*
+program closed  : 24/03/2020 tuesday IST 20:31
+github link     : github.com/SP3CTRE9886/ncrypter
+coded with      :c++
+auothor         :Nived.R.S
+*/
 #include <iostream>
 #include <cstring>
 #include <cstdio>
@@ -10,25 +16,27 @@ void decrypt();
 //----------------global variable declaration--------------------
 char str[30];//string for input and output
 int i/*for for loop*/,num=0/*for ASCII converition*/;
+int ConvLimit=0;
+int key;
 
-
+//----------------main program------------------
 int main() {
   int opt;
   cout<<"\t\t\t\t ---------Choose a option--------\n "
       <<"\t\t\t\t\t  [1]Encrypt\n"
       <<"\t\t\t\t\t  [2]Decrypt\n"
       <<"\t\t\t\t\t  [0]Exit\n"
-      <<"\t\t\t\t\t     OPTION >>> ";
- cin>>opt;
-if (opt==1)
+      <<"\n\t\t\t\t\t  OPTION >>> ";
+  cin>>opt;
+  if (opt==1)
       encrypt();
-else if (opt==2)
+  else if (opt==2)
       decrypt();
-else if (opt==0)
+  else if (opt==0)
       exit(0);
-else {
-    cout<<"Invalid option please try again !!!";
-    main();}
+  else {
+      cout<<"\t\t\t\t\tInvalid option please try again !!!";
+      main();}
   return 0;
 }
 
@@ -36,46 +44,82 @@ else {
 void encrypt(){  //encrypt function
     cout<<"Enter text to Encrypt : ";
 
-		for (i=0;i<250;i++)
+	for (i=0;i<250;i++)
 		{
-			str[i]=getchar();
-			if (i !=0 && str[i] == '\n')
-					break;
+	           str[i]=getchar();
+               if (i !=0 && str[i] == '\n')//stops reading when press ENTER
+                    break;
 		}
-
-	  for(i=0;i<250;i++){
+    ConvLimit = strlen(str);
+    //cout<<;  //--DEBUG PURPOSE
+    str[ConvLimit]='\0';
+    str[0]='\0';
+    str[ConvLimit-1]='\0';
+    cout<<"Encrypted Text is :: ";
+	for(i=1;i<(ConvLimit-1);i++){
       //-----------------
-      if (str[i]=='\0')
+      if (str[i]=='\0')//used for removing occational segmentation faults
           break;
-   //-----------------
+     //-----------------
+     key=i%4;
+     //-----------------
       num=str[i]; //letter to ASCII
-      num=num+1; //converting to next charecter
+      switch (key) {
+          case 0 :num++;
+                  break;
+          case 1 :num +=2;
+                  break;
+          case 2 :num += 3;
+                  break;
+          case 3 :num+= 4;
+                break;
+      } //converting to next charecter
       str[i]=num;//ASCII to letter
       cout<<str[i];//print new letter
   //-------------------
     }
+    cout<<endl<<'\a';
 	main();
 }
 
 
 void decrypt(){//decrypt function same as encrypt
-  cout<<"Enter text to Decrypt : ";
+    cout<<"Enter text to Decrypt : ";
 	for (i=0;i<250;i++)
 	{
 		str[i]=getchar();
 		if (i !=0 && str[i] == '\n')
 				break;
 	}
-  for(i=0;i<250;i++){
+    ConvLimit = strlen(str);
+    //cout<<ConvLimit;  //--DEBUG PURPOSE
+    str[ConvLimit]='\0';
+    str[ConvLimit-1]='\0';
+    str[0]='\0';
+    cout<<"Decrypted text is :: ";
 
-		if (str[i]=='\0')
-				break;
-
+    for(i=1;i<(ConvLimit-1);i++){
+//-------------------
+        if (str[i]=='\0')
+	       break;
+//-------------------
+    key=i % 4;
+//-------------------
     num=str[i];
-    num=num-1;
+    switch (key) {
+        case 0 :num--;
+                break;
+        case 1 :num -=2;
+                break;
+        case 2 :num -= 3;
+                break;
+        case 3 :num -= 4;
+              break;
+    }
     str[i]=num;
     cout<<str[i];
 //-------------------
   }
+    cout<<endl<<'\a';
 	main();
 }
--- start.py
+++ start.py
@@ -1,4 +1,5 @@
 import os
+from time import sleep
 os.system("resize -s 24 94 ")
 def clearterm():
 	os.system("clear")
@@ -15,10 +16,13 @@ print('''
                      | | | | |____| |  | |_| | |_) | ||  __/ |
                      |_| |_|\_____|_|   \__, | .__/ \__\___|_|
                                          __/ | |
-                                        |___/|_|   loading.....              ''')
+                                        |___/|_|   loading.....
+		ncrypter  Copyright (C) 2019-2020  Nived R S
+		This program comes with ABSOLUTELY NO WARRANTY.
+		This is free software, and you are welcome to redistribute it
+		under certain conditions ''')
 
 
-										
 os.system("g++ -Wall -c 'master.cpp' ")#compile
 os.system("g++ -Wall -o 'master' 'master.cpp' ")#build
 clearterm()
@@ -31,5 +35,22 @@ print('''                             _____                  _
                      | | | | |____| |  | |_| | |_) | ||  __/ |
                      |_| |_|\_____|_|   \__, | .__/ \__\___|_|
                                          __/ | |
-                                        |___/|_|                 ''')
+                                        |___/|_|    v1.0 stable             ''')
 os.system("./master")#run
+clearterm()
+print('''
+
+
+
+
+			               _____                  _
+                            / ____|                | |
+                      _ __ | |     _ __ _   _ _ __ | |_ ___ _ __
+                     | '_ \| |    | '__| | | | '_ \| __/ _ \ '__|
+                     | | | | |____| |  | |_| | |_) | ||  __/ |
+                     |_| |_|\_____|_|   \__, | .__/ \__\___|_|
+                                         __/ | |
+                                        |___/|_|   Thank you .....              ''')
+sleep(1)
+clearterm()
+os.system("exit")
