ea267355915c6640545c1c6b4f6e0d06daf4a1f8
n1ved <nivedrsalini@gmail.com>
2024-02-04 16:49:04 +0530

    Initial rewrite

--- ncrypter.cpp
+++ ncrypter.cpp
@@ -1,139 +1,184 @@
 #include <iostream>
-#include <cstdlib>
-#include <cstring>
-#include <cstdio>
+#include <string>
+#include <fstream>
+#include <sstream>
+
 using namespace std;
-//-----------------function declaration--------------------------
-void encrypt();
-void decrypt();
-void banner();
+
+bool isFileMode = true; // true = file, false = string
+string inputfile = "";
+string outputfile = "";
+string inputString = "";
+string outputString = "";
+int key = -1;
+bool isEncrypt = false;
+bool isDecrypt = false;
+bool fileout = false;
 
 
-//----------------global variable declaration--------------------
-char str[30];//string for input and output
-int i,num=0;
-int ConvLimit=0;
-int key;
+void printHelp();
+bool checkUsageErrors();
+bool encrypt(bool isFileMode, string input, int key);
+bool decrypt(bool isFileMode, string input, int key);
+char hashChar(char c, int key);
 
-//----------------main program------------------
-int main() {
-  int opt;
-  banner();
-  cout<<"\t\t\t ---------Choose an option--------\n "
-      <<"\t\t\t\t  [1]Encrypt\n"
-      <<"\t\t\t\t  [2]Decrypt\n"
-      <<"\t\t\t\t  [0]Exit\n"
-      <<"\n\t\t\t\t  OPTION >>> ";
-  cin>>opt;
-  if (opt==1)
-      encrypt();
-  else if (opt==2)
-      decrypt();
-  else if (opt==0){
-      system("clear");
-      exit(0);
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        printHelp();
+        return 1;
+    }
+    
+    for(int i=1; i<=argc; i++){
+        if(argv[i] == "--encrypt" || argv[i] == "-e")
+            isEncrypt = true;
+        else if(argv[i] == "--decrypt" || argv[i] == "-d")
+            isDecrypt = true;
+        else if(argv[i] == "--mode" || argv[i] == "-m"){
+            i++;
+            isFileMode = ( argv[i] == "t" ? false : true);
+        }
+        else if(argv[i] == "--key" || argv[i] == "-k"){
+            i++;
+            key = argv[i][0] - '0';
+        }
+        else if(argv[i] == "--output" || argv[i] == "-o"){
+            i++;
+            fileout = true;
+            outputfile = argv[i];
+        }
+        else{
+            if(fileout)
+                inputfile = argv[i];
+            else
+                inputString = argv[i];
+        }
+    }
+
+    if(checkUsageErrors())
+        return 1;
+    
+    if(isEncrypt){
+        isFileMode ? encrypt(true, inputfile, key) : encrypt(false, inputString, key);
+    }
+    else if(isDecrypt){
+        isFileMode ? decrypt(true, inputfile, key) : decrypt(false, inputString, key);
+    }
+    else{
+        cout << "Error: No mode of operation set" << endl;
+        return 1;
+    }
+
+
+} 
+
+void printHelp(){
+    cout << "ncrypter - a simple file encrypter" << endl;
+    cout << "Usage: ncrypter <option(s)> followed by FILENAME or STRING" << endl;
+    cout << "Where <option> is one of the following:" << endl;
+    cout << "  --encrypt ( -e ) : Encrypt the file" << endl;
+    cout << "  --decrypt ( -d ) : Decrypt the file" << endl;
+    cout << "  --help ( -h ) : Display this help message" << endl;
+    cout << "  --mode ( -m ) : Set mode of operation (File (f) or Text (t) )  " << endl;
+    cout << "  --key ( -k ) : Set the key for encryption/decryption" << endl;
+    cout << "  --output ( -o ) : Set the output file" << endl;
+    cout << "Example: ncrypter -e -m f -k mykey -o output.txt input.txt" << endl;
+}
+
+bool checkUsageErrors(){
+    if(isEncrypt && isDecrypt){
+        cout << "Error: Cannot encrypt and decrypt at the same time" << endl;
+        return true;
+    }
+    if(key == -1){
+        cout << "Error: Key not set" << endl;
+        return true;
+    }
+    if(fileout && outputfile == ""){
+        cout << "Error: Output file not set" << endl;
+        return true;
+    }
+    if(isFileMode && inputfile == ""){
+        cout << "Error: Input file not set" << endl;
+        return true;
+    }
+    if(!isFileMode && inputString == ""){
+        cout << "Error: Input string not set" << endl;
+        return true;
+    }
+    return false;
+}
+
+bool encrypt(bool isFileMode, string input, int key){
+    if(isFileMode){
+        ifstream file(input);
+        if(file.is_open()){
+            string line;
+            while(getline(file, line)){
+                for(int i=0; i<line.length(); i++){
+                    line[i] = hashChar(line[i], key);
+                }
+                outputString += line + "\n";
+                cout << line << endl;
             }
-  else {
-      cout<<"\t\t\t\t\tInvalid option please try again !!!";
-      main();}
-  return 0;
-}
-
-
-void encrypt(){  //encrypt function
-    cout<<"Enter text to Encrypt : ";
-
-	for (i=0;i<250;i++)
-		{
-	           str[i]=getchar();
-               if (i !=0 && str[i] == '\n')//stops reading when press ENTER
-                    break;
-		}
-    ConvLimit = strlen(str);
-    //cout<<;  //--DEBUG PURPOSE
-    str[ConvLimit]='\0';
-    str[0]='\0';
-    str[ConvLimit-1]='\0';
-    cout<<"Encrypted Text is :: ";
-	for(i=1;i<(ConvLimit-1);i++){
-      //-----------------
-      if (str[i]=='\0')//used for removing occational segmentation faults
-          break;
-     //-----------------
-     key=i%4;
-     //-----------------
-      num=str[i]; //letter to ASCII
-      switch (key) {
-          case 0 :num++;
-                  break;
-          case 1 :num +=2;
-                  break;
-          case 2 :num += 3;
-                  break;
-          case 3 :num+= 4;
-                break;
-      } //converting to next charecter
-      str[i]=num;//ASCII to letter
-      cout<<str[i];//print new letter
-  //-------------------
+            file.close();
+        }
+        else{
+            cout << "Error: Unable to open file" << endl;
+            return false;
+        }
     }
-    cout<<endl<<'\a';
-	main();
-}
-
-
-void decrypt(){//decrypt function same as encrypt
-    cout<<"Enter text to Decrypt : ";
-	for (i=0;i<250;i++)
-	{
-		str[i]=getchar();
-		if (i !=0 && str[i] == '\n')
-				break;
-	}
-    ConvLimit = strlen(str);
-    str[ConvLimit]='\0';
-    str[ConvLimit-1]='\0';
-    str[0]='\0';
-    cout<<"Decrypted text is :: ";
-
-    for(i=1;i<(ConvLimit-1);i++){
-//-------------------
-        if (str[i]=='\0')
-	       break;
-//-------------------
-    key=i % 4;
-//-------------------
-    num=str[i];
-    switch (key) {
-        case 0 :num--;
-                break;
-        case 1 :num -=2;
-                break;
-        case 2 :num -= 3;
-                break;
-        case 3 :num -= 4;
-              break;
+    else{
+        for(int i=0; i<input.length(); i++){
+            input[i] = hashChar(input[i], key);
+        }
+        outputString = input;
+        cout << input << endl;
     }
-    str[i]=num;
-    cout<<str[i];
-//-------------------
-  }
-    cout<<endl<<'\a';
-	main();
+    return true;
 }
 
-// ---------------------------Welcome banner------------------------------------ 
+char hashChar(char c, int key){
+    if(isalpha(c)){
+        if(isupper(c)){
+            c = (c + key - 65) % 26 + 65;
+        }
+        else{
+            c = (c + key - 97) % 26 + 97;
+        }
+    }
+    else if(isdigit(c)){
+        c = (c + key - 48) % 10 + 48;
+    }
+    return c;
+}
 
-void banner(){
-    cout<<"\t                                                             \n"
-        <<"\t                                                             \n"
-    	<<"\t	             _____                  _                     \n"
-        <<"\t                    / ____|                | |               \n"
-        <<"\t              _ __ | |     _ __ _   _ _ __ | |_ ___ _ __     \n"
-        <<"\t             | '_ \\| |    | '__| | | | '_ \\| __/ _ \\ '__| \n"
-        <<"\t             | | | | |____| |  | |_| | |_) | ||  __/ |       \n"
-        <<"\t             |_| |_|\\_____|_|   \\__, | .__/ \\__\\___|_|   \n"
-        <<"\t                                 __/ | |                     \n"
-        <<"\t                                |___/|_| v1.1                \n"
-        <<"\t                                                             \n"; 
+bool decrypt(bool isFileMode, string input, int key){
+    if(isFileMode){
+        ifstream file(input);
+        if(file.is_open()){
+            string line;
+            while(getline(file, line)){
+                for(int i=0; i<line.length(); i++){
+                    line[i] = hashChar(line[i], -key);
+                }
+                outputString += line + "\n";
+                cout << line << endl;
+            }
+            file.close();
+        }
+        else{
+            cout << "Error: Unable to open file" << endl;
+            return false;
+        }
+    }
+    else{
+        for(int i=0; i<input.length(); i++){
+            input[i] = hashChar(input[i], -key);
+        }
+        outputString = input;
+        cout << input << endl;
+    }
+    return true;
 }
